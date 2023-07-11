176741eef9216d3c206379b98ed404834b4239d4
LPraveenB <praveen.bl@kloud9.nyc>
2023-07-11 12:23:42 +0530

    Build shell

---
+++ build.sh
@@ -0,0 +1,10 @@
+docker build --no-cache -t dask-mlpipeline-image:latest .
+
+gcloud auth configure-docker us-west1-docker.pkg.dev
+
+CLOUD_REPOSITORY="us-west1-docker.pkg.dev/inventory-solution-382204/dask-docker"
+echo "Push to Container Registry: version taged as latest"
+docker tag dask-mlpipeline-image:latest ${CLOUD_REPOSITORY}/dask-mlpipeline-image:latest
+docker push ${CLOUD_REPOSITORY}/dask-mlpipeline-image:latest
+
+
