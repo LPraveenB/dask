93ba1e125a8d3abf7153ac9bfee0000bb4baee03
LPraveenB <praveen.bl@kloud9.nyc>
2023-08-02 15:34:07 +0530

    Credentials

--- build.sh
+++ build.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
+set -e
 
-# Save the GCP_SA_KEY secret to a temporary file
-echo "$GCP_SA_KEY" > temp_gcp_sa_key.json
+# Copy secret to a temporary file
+printf '%s' "$1" > temp_gcp_sa_key.json
 
-# Escape double quotes in the secret JSON and save it to gcp_sa_key.json
+# Escape double quotes and save to /train/gcp_sa_key.json
 cat temp_gcp_sa_key.json | sed 's/"/\\"/g' > /train/gcp_sa_key.json
 
-# Build the Docker image with the GCP_SA_KEY as an argument
-docker build --no-cache --build-arg GCP_SA_KEY=$(cat temp_gcp_sa_key.json) -t dask-mlpipeline-image:latest .
+# Build Docker image
+docker build --no-cache -t dask-mlpipeline-image:latest .
 
-# Clean up the temporary file
-rm temp_gcp_sa_key.json
-
-
+# Authenticate Docker to Google Container Registry
 gcloud auth configure-docker us-west1-docker.pkg.dev
 
+# Push Docker image to Google Container Registry
 CLOUD_REPOSITORY="us-west1-docker.pkg.dev/inventory-solution-382204/dask-docker"
-echo "Push to Container Registry: version taged as latest"
+echo "Push to Container Registry: version tagged as latest"
 docker tag dask-mlpipeline-image:latest ${CLOUD_REPOSITORY}/dask-mlpipeline-image:latest
 docker push ${CLOUD_REPOSITORY}/dask-mlpipeline-image:latest
