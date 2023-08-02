9377c07b82c94a47598226cdefc3ff4b1c4f1b1f
LPraveenB <praveen.bl@kloud9.nyc>
2023-08-02 14:46:50 +0530

    Credentials

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
-docker build --no-cache --build-arg GCR_AUTH_KEY="$1" -t dask-mlpipeline-image:latest .
+#!/bin/bash
+
+# Save the GCP_SA_KEY secret to a temporary file
+echo "$GCP_SA_KEY" > temp_gcp_sa_key.json
+
+# Escape double quotes in the secret JSON and save it to gcp_sa_key.json
+cat temp_gcp_sa_key.json | sed 's/"/\\"/g' > /train/gcp_sa_key.json
+
+# Build the Docker image with the GCP_SA_KEY as an argument
+docker build --no-cache --build-arg GCP_SA_KEY=$(cat temp_gcp_sa_key.json) -t dask-mlpipeline-image:latest .
+
+# Clean up the temporary file
+rm temp_gcp_sa_key.json
+
 
 gcloud auth configure-docker us-west1-docker.pkg.dev
 
